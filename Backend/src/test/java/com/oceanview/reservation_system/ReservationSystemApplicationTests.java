279d1add424e4f68469d7068156f8aebb9730195
Kevin Thulnith <112649346+kevinThulnith@users.noreply.github.com>
2026-02-28 21:13:24 +0530

    project initilize

---
+++ Backend/src/test/java/com/oceanview/reservation_system/ReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oceanview.reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
